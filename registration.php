9d4b7193b26371bd62406e26dfef6d39a428dd65
GirishDarbar <36296483+GirishDarbar@users.noreply.github.com>
2018-02-09 17:22:02 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Romcom_Girish',
+        __DIR__
+    );
+    ?>
